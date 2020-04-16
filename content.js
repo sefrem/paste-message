792ad062d9ed93f27394fb692192c78ed7c5ede1
sefrem <fireweb2112@gmail.com>
2020-04-16 23:24:13 +0400

    sending fixed

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 chrome.runtime.onMessage.addListener(request=> {
   if (request.message === "Extension tab") {
     document.execCommand("paste");
+    document.querySelector('[aria-label="Отправить"]').click()
   }
 });
+
