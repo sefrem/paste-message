51b049c5878f6cea6d817cba6a3cf7711d691e84
sefrem <fireweb2112@gmail.com>
2020-04-17 16:04:44 +0400

    still fixing sending

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === "Extension tab") {
-    navigator.clipboard
-      .readText()
-      .then(() => {
-        document.execCommand("paste");
-      })
-      .then(() => document.querySelector('[aria-label="Отправить"]').click());
+    navigator.clipboard.readText().then(() => {
+      document.execCommand("paste");
+      setTimeout(function () {
+        document.querySelector('[aria-label="Отправить"]').click();
+      }, 0);
+    });
   }
 });
