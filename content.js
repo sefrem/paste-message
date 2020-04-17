d9203a4418f2831addd772b2883b84fd3ede332b
sefrem <fireweb2112@gmail.com>
2020-04-17 17:32:19 +0400

    added mutation observer for sending messages

--- content.js
+++ content.js
@@ -1,10 +1,25 @@
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === "Extension tab") {
+    observer.observe(document, config);
     navigator.clipboard.readText().then(() => {
       document.execCommand("paste");
-      setTimeout(function () {
-        document.querySelector('[aria-label="Отправить"]').click();
-      }, 0);
     });
   }
 });
+
+const observer = new MutationObserver((mutations) => {
+  for (let mutation of mutations) {
+    if (
+      mutation.addedNodes.length &&
+      mutation.addedNodes[0].ariaLabel === "Отправить"
+    ) {
+      mutation.addedNodes[0].click();
+      observer.disconnect();
+    }
+  }
+});
+
+const config = {
+  childList: true,
+  subtree: true,
+};
